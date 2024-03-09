3849dd60ab39ed07b7cf612c824231896112d5ca
guro199811 <guro.sivs@gmail.com>
2024-03-09 11:13:08 +0400

    feat: Addition to main.py
    Ideas to work with

--- workout-api/main.py
+++ workout-api/main.py
@@ -32,6 +32,7 @@ db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
 
 
+# For User Menu, Contains User Data
 @app.get("/user", status_code=status.HTTP_200_OK)
 async def user(user: user_dependency, db: db_dependency):
     if user is None:
@@ -39,18 +40,54 @@ async def user(user: user_dependency, db: db_dependency):
     return {"User": user}
 
 
+
+# For Main Page, Contains Exercises for displaying on main page
 @app.get("/", status_code=status.HTTP_200_OK)
-async def home(db):
+async def main(db):
     exercises = db.query(models.Exercise).all()
     return {"exercises": exercises }
 
 
+
+# Workouts Urls, For Getting data, Posting data, Editing data and Deleting data
 @app.get("/user/personal_workout/{user_id}")
-def workouts(user: user_dependency, db: db_dependency):
+def workouts_get(user: user_dependency, db: db_dependency):
     pass
 
 
+@app.post("/user/personal_workout/{user_id}")
+def workouts_post(user: user_dependency, db: db_dependency):
+    pass
+
+
+@app.put("/user/personal_workout/{user_id}")
+def workouts_put(user: user_dependency, db: db_dependency):
+    pass
+
+
+@app.delete("/user/personal_workout/{user_id}")
+def workouts_delete(user: user_dependency, db: db_dependency):
+    pass
+
+
+
+# Goals Urls, For Getting, Posting, Editing and Deleting
 @app.get("/user/personal_goals/{user_id}")
-def goals(user: user_dependency, db: db_dependency):
+def goals_get(user: user_dependency, db: db_dependency):
+    pass
+
+
+@app.post("/user/personal_goals/{user_id}")
+def goals_post(user: user_dependency, db: db_dependency):
+    pass
+
+
+@app.put("/user/personal_goals/{user_id}")
+def goals_put(user: user_dependency, db: db_dependency):
+    pass
+
+
+@app.delete("/user/personal_goals/{user_id}")
+def goals_delete(user: user_dependency, db: db_dependency):
     pass
 
