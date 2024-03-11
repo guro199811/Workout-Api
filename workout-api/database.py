2b6a8b22ab437610f053f6d9fa736339bc48f200
guro199811 <guro.sivs@gmail.com>
2024-03-11 14:32:53 +0400

    feat: database creation on container startup

--- workout-api/database.py
+++ workout-api/database.py
@@ -1,11 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import database_exists, create_database
 
 SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@postgresql:5432/workoutdb'
 
-
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Create database if it doesn't exist
+if not database_exists(engine.url):
+    create_database(engine.url)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
