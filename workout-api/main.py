2c1d0d0c3a2395c3cd8b513c07e3dd6912063b2d
guro199811 <guro.sivs@gmail.com>
2024-03-13 12:28:51 +0400

    fix: main.py
    adjusted for new router names

--- workout-api/main.py
+++ workout-api/main.py
@@ -3,8 +3,8 @@ from database import engine, Base
 from seed import populate_database
 
 from routes import (
-    auth, exercise, goal,
-    history, schedule, user
+    auth, exercise_routes, goal_routes,
+    history_routes, schedule_routes, user_routes
 )
 
 
@@ -14,11 +14,11 @@ app = FastAPI()
 
 # Adding Auth Router
 app.include_router(auth.auth)
-app.include_router(user.user_route)
-app.include_router(exercise.exercise)
-app.include_router(goal.goal)
-app.include_router(schedule.schedule)
-app.include_router(history.hist)
+app.include_router(user_routes.user_route)
+app.include_router(exercise_routes.exercise)
+app.include_router(goal_routes.goal)
+app.include_router(schedule_routes.schedule)
+app.include_router(history_routes.hist)
 
 
 
